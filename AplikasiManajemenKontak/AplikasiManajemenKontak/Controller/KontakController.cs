2ddd5edc04e35973f783d123ba4007576b1ba3c5
YolendesHermadiDandi <marselius306@gmail.com>
2023-09-15 12:29:06 +0700

    create user

--- AplikasiManajemenKontak/AplikasiManajemenKontak/Controller/KontakController.cs
+++ AplikasiManajemenKontak/AplikasiManajemenKontak/Controller/KontakController.cs
@@ -10,27 +10,107 @@ namespace AplikasiManajemenKontak.Controller
     public class KontakController
     {
 
-        private static List<Kontak> daftarKontak = new List<Kontak>(); 
-        public static void CreateKontak() 
+        private static List<Kontak> daftarKontak = new List<Kontak> { };
+        private static int id = 0;
+        public static void CreateKontak()
         {
 
-        
+            Kontak user = InputValidation();
+            daftarKontak.Add(user);
+            Console.WriteLine("Data Berhasil Disimpan");
+            String input = Console.ReadLine();
+
         }
 
 
         private static Kontak InputValidation()
         {
+            string name = InputCheck("Name : ");
+            string phoneNumber = InputCheck("Phone Number : ");
+            string emailAddress = InputCheck("Email : ");
 
 
-            Kontak kontak = new Kontak();   
+
+
+            int idCount = ++id;
+            Kontak kontak = new Kontak(idCount.ToString(), name, phoneNumber, emailAddress);
+
             return kontak;
-    
-          
+
+
         }
 
         private static String InputCheck(String inputType)
         {
-            return "asas";
+            Boolean checkvalidation = true;
+            String input = "";
+
+            do
+            {
+                if (inputType.Equals("Name : "))
+                {
+                    Console.Write(inputType);
+                    input = Console.ReadLine();
+                    Boolean nameCheck = true;
+                    foreach (var name in daftarKontak)
+                    {
+                        if (name.Name == input)
+                        {
+                            nameCheck = false;
+                        }
+
+                    }
+                    if (nameCheck == false)
+                    {
+                        Console.WriteLine("NAMA TIDAK BOLEH SAMA!!!");
+                    }
+                    else
+                    {
+                        checkvalidation = false;
+                    }
+
+                }
+                else if (inputType.Equals("Phone Number : "))
+                {
+                    Console.Write(inputType);
+                    input = Console.ReadLine();
+                    if (input.All(char.IsDigit))
+                    {
+                        if (input.Length < 8 ||
+                            input.Length > 15)
+                        {
+                            Console.WriteLine("INPUTAN TIDAK VALID !!!");
+                        }
+                        else
+                        {
+                            checkvalidation = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("INPUTAN TIDAK VALID !!!");
+                    }
+                }
+                else if (inputType.Equals("Email : "))
+                {
+                    Console.Write(inputType);
+                    input = Console.ReadLine();
+
+                    if (!input.Contains("@gmail.com"))
+                    {
+                        Console.WriteLine("EMAIL TIDAK VALID!!!");
+                    }
+                    else
+                    {
+                        checkvalidation = false;
+                    }
+                }
+
+
+            } while (checkvalidation);
+
+
+            return input;
 
         }
     }
--- AplikasiManajemenKontak/AplikasiManajemenKontak/Program.cs
+++ AplikasiManajemenKontak/AplikasiManajemenKontak/Program.cs
@@ -1,9 +1,11 @@
+using AplikasiManajemenKontak.View;
+
 namespace AplikasiManajemenKontak;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("testing");
+        View.View.vBaseMenu();
     }
 }
