a9996b5cadb65d69707fc4b610614275d8537199
YolendesHermadiDandi <marselius306@gmail.com>
2023-09-15 11:42:46 +0700

    update controller

--- AplikasiManajemenKontak/AplikasiManajemenKontak/Controller/KontakController.cs
+++ AplikasiManajemenKontak/AplikasiManajemenKontak/Controller/KontakController.cs
@@ -20,84 +20,17 @@ namespace AplikasiManajemenKontak.Controller
 
         private static Kontak InputValidation()
         {
-            String firstName;
-            String lastName;
-            String password;
-
-            //Cek Validasi
-            firstName = InputCheck("First Name : "); //cek inputan first name
-            lastName = InputCheck("Last Name : "); // cek inputan last name
-            password = InputCheck("Password : "); // cek inputan password
 
 
-            //membuat username secara otomatis 
-            String username = firstName.Substring(0, 2) + "" + lastName.Substring(0, 2);
-            foreach (var item in daftarKaryawan)
-            {
-                if (item.Value.Username == username)
-                {
-
-                    username = username + "" + (++usernamecount);
-                }
-            }
-
-
-            Kontak user = new Kontak();
-
-            return user;
+            Kontak kontak = new Kontak();   
+            return kontak;
+    
+          
         }
 
         private static String InputCheck(String inputType)
         {
-            Boolean checkvalidation = true;
-            String input = "";
-
-            do
-            {
-                if (inputType.Equals("Name : "))
-                {
-                    Console.Write(inputType);
-                    input = Console.ReadLine();
-                    if (string.IsNullOrEmpty(input) || input.Any(Char.IsDigit) || input.Length < 2)
-                    {
-                        Console.WriteLine("INPUTAN TIDAK FALID");
-
-                    }
-                    else
-                    {
-                        checkvalidation = false;
-                    }
-                }else if (inputType.Equals("Email : "))
-                {
-                    Console.Write("Password : ");
-                    input = Console.ReadLine();
-                    if (input.Any(Char.Equals("@gmail.com")){
-
-                    }
-                }
-             
-                else if (inputType.Equals("Password : "))
-                {
-                    Console.Write("Password : ");
-                    input = Console.ReadLine();
-                    if (input.Length < 8 ||
-                        !input.Any(Char.IsDigit) ||
-                        !input.Any(Char.IsUpper) || !input.Any(Char.IsLower))
-                    {
-                        Console.WriteLine("PASSWORD MINIMAL MEMILIKI 8 KARAKTER !!");
-                        Console.WriteLine("PASSWORD HARUS TERDIRI DARI HURUF BESAR, HURUF KECIL DAN ANGKA");
-                    }
-                    else
-                    {
-                        checkvalidation = false;
-                    }
-
-                }
-
-
-            } while (checkvalidation);
-
-            return input;
+            return "asas";
 
         }
     }
